674d9cf252114eddffd99c085e8bf11213b27a7b
StephenHoman <jakenesler@gmail.com>
2023-03-13 17:41:00 -0400

    added Item image creation
    - deleted page create_item.php
    - Moved all contents to user_page.php and it can be accessed through a modal,
    - activate modal line 84
    - Item creation form is now at line 334
    
    Changed some of the general functionality, Will need to eventually  change to prepared statements

--- insert_item.php
+++ insert_item.php
@@ -1,30 +1,117 @@
 <?php
-    // Initialize the session
-    session_start();
-   
-?>
-<?php
-    require_once("dBCred.PHP");
-?>
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <title>Store Item Data</title>
-   </head>
+ 
+require_once("dBCred.PHP");
 
-   <body>
-        <center>
-        <?php
-            $item_name = $_REQUEST['name'];
-            $item_description = $_REQUEST['description'];
-            $price = $_REQUEST['price'];
-            $category = $_REQUEST['category'];
-            $tags = $_REQUEST['tags'];
+$item_name = $price = $category = $tags = '';
+$image_id = $user_id = $category_id = $tag_id = $premium_status = $sold = 0;
+$date_posted = date("Y-m-d");
+$featured_item = 0;
+
+function uploadProfileImage($photoInput, $conn)  {
+    $uploadDir = 'photos/item_image';
+    $tempName = $photoInput['tmp_name'];
+    $fileName = basename($photoInput['name']);
+    $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    $hashedName = hash('sha256', time() . $fileName) . '.' . $fileType;
+    $targetFile = $uploadDir . '/' . $hashedName;
+    $directory =  $targetFile;
+    $_SESSION['image_url'] = $targetFile;
+    require_once("dBCred.PHP");
+
+    // Move uploaded file to new location with new name
+    if(move_uploaded_file($tempName, $targetFile)){
+        // Prepare an insert statement
+     
+        $sql = "INSERT INTO mydatabase.images (image_id, image_url) VALUES (NULL, ?)";
+     
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_image_url);
+             
+            // Set parameters
+            $param_image_url = $directory;
+        }
+        else{
+            echo "failed at line 24";
+        }
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt))
+            { echo mysqli_error($conn);
+                return $directory;
+            }
+            else
+            {echo mysqli_error($conn);
+                echo "image Failed- sql error";
+            }
+        }
+     
+        return 'photos/profile_image/default.webp';
+    
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{ 
+    if (isset($_FILES['itemFormFile']) && $_FILES['itemFormFile']['error'] == UPLOAD_ERR_OK) {
+        // File was uploaded successfully
+        $newFileName = uploadProfileImage($_FILES['itemFormFile'], $conn);
+        $_SESSION['item_image_url'] = $newFileName;
+        if ($newFileName) {
+            // File was uploaded successfully
+            echo 'File was uploaded successfully with name ' . $newFileName;
+            $sql = "SELECT image_id FROM mydatabase.images WHERE image_url = ?";
+            if($stmt = mysqli_prepare($conn, $sql))
+            {
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_image_url);
+                $param_image_url =  $newFileName  ;
+                if(mysqli_stmt_execute($stmt))
+                { 
+                    // Store result
+                    mysqli_stmt_store_result($stmt);
+                    // Check if username exists, if yes then verify password
+                    if(mysqli_stmt_num_rows($stmt) == 1)
+                    {                    
+                        // Bind result variables
+                        mysqli_stmt_bind_result($stmt, $image_id);
+                        while (mysqli_stmt_fetch($stmt)) {
+                            $_SESSION['item_image_id'] = $image_id;
+            
+                                        }
+                                    }}}
+                        } else {
+                            echo 'Error moving uploaded file.';
+            
+                        }
+                        
+            
+                    }
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            $item_name =  trim($_POST['name']);
+            $item_description =  trim($_POST['description']);
+            $price =  trim($_POST['price']);
+            $category =  trim($_POST['category']);
+            $tags =  trim($_POST['tags']);
             // $featured_item = $_REQUEST['featured'];
 
             // hardcoded ids for now to get it to work
-            $image_id = 100;
-            $user_id = $_SESSION['id'];
+            $image_id = $_SESSION['item_image_id'];
+            $user_id = trim($_POST['user_id']);
+
             $category_id = 101;
             $tag_id = 104;
             $date_posted = date("Y-m-d");
@@ -42,7 +129,7 @@
         
                 $_SESSION['message'] = "item been created sucessfully!";
                 mysqli_close($conn);
-                header("Location: create_item.php");
+                header("Location: user_page.php");
                 exit;
                 
                 } else {
@@ -50,8 +137,13 @@
                 }         
                   
 
+
+ 
+                    }
+                
+
+
+
+
         ?>
         
-    </body>
-
-</html>
--- user_page.php
+++ user_page.php
@@ -1,19 +1,17 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-   header("location: index.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: index.php");
+    exit();
 }
 ?>
 <?php
-    require_once("dBCred.PHP");
-    require_once("php_update_user.php");
-     
- 
-  ?>
+require_once "dBCred.PHP";
+require_once "php_update_user.php";
+?>
 
 <!doctype html>
 <html lang="en">
@@ -57,12 +55,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                 <img src="photos/fillerlogo.jpg" class="img-rounded img-profile embed-responsive " id="profile" alt="profile image"> 
                 </a>
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">#<?php echo $_SESSION['id']; ?> - <?php echo $_SESSION['username']; ?> </span>
+                    <span class="fs-5 d-none d-sm-inline">#<?php echo $_SESSION[
+                        "id"
+                    ]; ?> - <?php echo $_SESSION["username"]; ?> </span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
                         <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"><?php echo $_SESSION['username']; ?></span>
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"><?php echo $_SESSION[
+                                "username"
+                            ]; ?></span>
                         </a>
                     </li>
                     <li>
@@ -78,10 +80,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                         </ul>
                     </li>
  
-                    <li>
-                        <a href="create_item.php" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 nav-link px-0 align-middle d-sm-inline">List Item</span></a>
-                    </li>
+                    <li class="w-100">
+       <!-- userModal button --> <a href="#" class="nav-link px-0"> <span class="ms-1 nav-link px-0 align-middle d-sm-inline"><button type="button" class="styledBtn btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemModal">List Item</button></span></a>
+                            </li>
                     <li  class="w-100">
                         <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
                             <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 nav-link px-0 align-middle d-sm-inline">Bootstrap</span></a>
@@ -119,10 +120,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
             
         <div class="form-group mb-3">
-        <?php  if (isset($_SESSION['username'])) : ?>
-             <?php echo $_SESSION['id']; ?> 
-             <?php echo $_SESSION['username']; ?> 
-             <?php endif ?>     
+        <?php if (isset($_SESSION["username"])): ?>
+             <?php echo $_SESSION["id"]; ?> 
+             <?php echo $_SESSION["username"]; ?> 
+             <?php endif; ?>     
 
         </div>
         
@@ -130,78 +131,91 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <?php
             // Prepare a select statement
             $sql = "SELECT * FROM mydatabase.users WHERE login_id = ?";
-        if($stmt = mysqli_prepare($conn, $sql))
-        {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_id);
-            $param_id = $_SESSION['id'];
-            if(mysqli_stmt_execute($stmt))
-            { 
-                 
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1)
-                {                    
-                     
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $user_id, $user_description, $email, $login_id, $image_id, $address_line_one, $address_line_two, $state, $city, $zip_code, $account_creation_date, $last_online, $transaction_count, $premium);
-                    
-                    while (mysqli_stmt_fetch($stmt)) {
-                        printf(" %d %s %s %d %d %s %s %s %s %d %d %d %d %d \n", $user_id, $user_description, $email, $login_id, $image_id, $address_line_one, $address_line_two, $state, $city, $zip_code, $account_creation_date, $last_online, $transaction_count, $premium);
-                        
-                        echo $email ."<br>";
-                        $_SESSION['USERID'] = $user_id;
-                        $_SESSION['LOGINID'] = $login_id;
-                        $_SESSION['IMAGEID'] = $image_id;
-                        
-                        
-                    }
+            if ($stmt = mysqli_prepare($conn, $sql)) {
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_id);
+                $param_id = $_SESSION["id"];
+                if (mysqli_stmt_execute($stmt)) {
+                    // Store result
+                    mysqli_stmt_store_result($stmt); // Check if username exists, if yes then verify password
+                    if (mysqli_stmt_num_rows($stmt) == 1) {
+                        // Bind result variables
+                        mysqli_stmt_bind_result(
+                            $stmt,
+                            $user_id,
+                            $user_description,
+                            $email,
+                            $login_id,
+                            $image_id,
+                            $address_line_one,
+                            $address_line_two,
+                            $state,
+                            $city,
+                            $zip_code,
+                            $account_creation_date,
+                            $last_online,
 
+                            $transaction_count,
+                            $premium
+                        );
+                        while (mysqli_stmt_fetch($stmt)) {
+                            printf(
+                                " %d %s %s %d %d %s %s %s %s %d %d %d %d %d \n",
+                                $user_id,
+                                $user_description,
+                                $email,
+                                $login_id,
+                                $image_id,
+                                $address_line_one,
+                                $address_line_two,
+                                $state,
+                                $city,
+                                $zip_code,
+                                $account_creation_date,
+                                $last_online,
+
+                                $transaction_count,
+                                $premium
+                            );
+                            echo $email . "<br>";
+                            $_SESSION["USERID"] = $user_id;
+                            $_SESSION["LOGINID"] = $login_id;
+                            $_SESSION["IMAGEID"] = $image_id;
+                        }
+                        mysqli_stmt_close($stmt);
+                    }
+                }
+            }
+            $sql = "SELECT image_url FROM mydatabase.images WHERE image_id = ?";
+            if ($stmt = mysqli_prepare($conn, $sql)) {
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_id);
+                $param_id = $image_id;
+                if (mysqli_stmt_execute($stmt)) {
+                    // Store result
+                    mysqli_stmt_store_result($stmt); // Check if username exists, if yes then verify password
+                    if (mysqli_stmt_num_rows($stmt) == 1) {
+                        // Bind result variables
+                        mysqli_stmt_bind_result($stmt, $image_url);
+                        while (mysqli_stmt_fetch($stmt)) {
+                            $_SESSION["image_url"] = $image_url;
+                            printf("%s \n", $image_url);
+                        }
+                    }
                     mysqli_stmt_close($stmt);
-                    
-                       
                 }
             }
-        }
-
-        $sql = "SELECT image_url FROM mydatabase.images WHERE image_id = ?";
-        if($stmt = mysqli_prepare($conn, $sql))
-        {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_id);
-            $param_id = $image_id;
-            if(mysqli_stmt_execute($stmt))
-            { 
-                 
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1)
-                {                    
-                     
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $image_url);
-                    while (mysqli_stmt_fetch($stmt)) {
-                        $_SESSION['image_url'] = $image_url;
-                        printf("%s \n", $image_url);
-                         
-                    }
-                }
-                mysqli_stmt_close($stmt);
-            }
-        }
-
-        require_once('checkFK.php');
-
-                ?>
+            require_once "checkFK.php";
+            ?>
         </div>
         <div class="col py-3">
              
             <div class="container text-center">
             <div class="row">
             <div class="col">
-            <img src="<?php echo $_SESSION['image_url']; ?>" class="img-rounded img-profile embed-responsive " id="profile" alt="profile image"> 
+            <img src="<?php echo $_SESSION[
+                "image_url"
+            ]; ?>" class="img-rounded img-profile embed-responsive " id="profile" alt="profile image"> 
             </div>
             <div class="col">
             Column
@@ -233,14 +247,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <h4 class="modal-title">Update Info</h4>
         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
       </div>
-      <?php
-     
-  ?>
+      <?php  ?>
       <!-- Modal body -->
       <div class="modal-body">
        
-         <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
-           <h3 class="mb-4"> <?php echo $_SESSION['username']; ?> </h3>
+         <form action="<?= htmlspecialchars(
+             $_SERVER["PHP_SELF"]
+         ) ?>" method="POST" enctype="multipart/form-data">
+           <h3 class="mb-4"> <?php echo $_SESSION["username"]; ?> </h3>
              
  
            <div class="mb-3">
@@ -254,42 +268,42 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
            <div class="mb-3 row">
                 <label for="staticEmail" class="col-sm-5 col-form-label">Email</label>
             <div class="col-sm-7">
-                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?=$email;?>">
+                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?= $email ?>">
             </div>
             </div>
             
             <div class="mb-3 row">
             <label for="inputPassword" class="col-sm-5 col-form-label">Address Line One</label>
             <div class="col-sm-7">
-            <input type="text" class="form-control" id="addressLineOne" name="addressLineOne" placeholder="<?=$address_line_one;?>">
+            <input type="text" class="form-control" id="addressLineOne" name="addressLineOne" placeholder="<?= $address_line_one ?>">
             </div>
             </div>
 
             <div class="mb-3 row">
             <label for="inputPassword" class="col-sm-5 col-form-label">Address Line Two</label>
             <div class="col-sm-7">
-            <input type="text" class="form-control" id="addressLineTwo" name="addressLineTwo" placeholder="<?=$address_line_two;?>">
+            <input type="text" class="form-control" id="addressLineTwo" name="addressLineTwo" placeholder="<?= $address_line_two ?>">
             </div>
             </div>
 
             <div class="mb-3 row">
             <label for="inputPassword" class="col-sm-1 col-form-label">State</label>
             <div class="col-sm-2">
-            <input type="text" class="form-control" id="state" name="state"  placeholder="<?=$state;?>">
+            <input type="text" class="form-control" id="state" name="state"  placeholder="<?= $state ?>">
             </div>
             <label for="inputPassword" class="col-sm-1 col-form-label">City</label>
             <div class="col-sm-4">
-            <input type="text" class="form-control" id="city" name="city"  placeholder="<?=$city;?>">
+            <input type="text" class="form-control" id="city" name="city"  placeholder="<?= $city ?>">
             </div>
             <label for="inputPassword" class="col-sm-1 col-form-label">Zip</label>
             <div class="col-sm-3">
-            <input type="text" class="form-control" id="zip"name="zip" placeholder="<?=$zip_code;?>">
+            <input type="text" class="form-control" id="zip"name="zip" placeholder="<?= $zip_code ?>">
             </div>
             </div>
 
             <div class="g-3 row">
             <label for="description" class="col-sm-12 col-form-label">User Description</label>
-            <textarea class="form-control" maxlength="200" rows="4" id="userDescription" name="userDescription" placeholder="<?=$user_description;?>"></textarea> 
+            <textarea class="form-control" maxlength="200" rows="4" id="userDescription" name="userDescription" placeholder="<?= $user_description ?>"></textarea> 
             <p class="h6 text-muted small">limit 200 Characters</p>
             </div>
 
@@ -311,6 +325,105 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </div>
 <!-- user info update modal end --> 
 
+
+
+
+
+
+
+<!-- Item List modal start --> 
+<!-- The Modal -->
+<div class="modal" id="itemModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">List Item </h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <?php require_once "insert_item.php"; ?>
+      <!-- Modal body -->
+      <div class="modal-body">
+       
+          <h3 class="mb-4"> <?php echo $_SESSION["username"]; ?> </h3>
+          <div class="text-center mb-3">
+								<div class="card">
+									<div class="card-header"> Create an Item </div>
+									<div class="card-body">
+
+                                        <!-- Start of form -->
+										<form action="insert_item.php" method="POST" enctype="multipart/form-data">
+                                        <input type="hidden" name="user_id" value="<?php echo $_SESSION[
+                                            "USERID"
+                                        ]; ?>">
+    <div class="mb-3">
+        <label for="name" class="form-label">Item Name:</label>
+        <input name="name" type="text" class="form-control" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="itemFormFile" class="form-label">Upload a new Profile Image</label>
+        <input class="form-control" type="file" id="itemFormFile" name="itemFormFile">
+    </div>
+
+    <div class="mb-3">
+        <label for="description" class="form-label">Item Description:</label>
+        <textarea name="description" cols="45" rows="3" class="form-control" required></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label for="price" class="form-label">Price:*</label>
+        <div class="input-group">
+            <span class="input-group-text">$</span>
+            <input name="price" type="number" min="0.00" step="0.01" class="form-control" required>
+        </div>
+    </div>
+
+    <div class="mb-3">
+        <label for="category" class="form-label">Item Category:*</label>
+        <select name="category" class="form-select" required>
+            <option value="" disabled selected>Select a Category</option>
+            <option value="1">Tech</option>
+            <option value="2">Other</option>
+        </select>
+    </div>
+
+    <div class="mb-3">
+        <label for="tags" class="form-label">Item Tags:</label>
+        <textarea name="tags" cols="30" rows="2" class="form-control" placeholder="Please separate each tag with a comma and a space." required></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label for="featured" class="form-label">Make this your profile's featured item?:</label>
+        <div class="form-check">
+            <input name="featured" type="radio" value="yay" id="featured-yes" class="form-check-input">
+            <label for="featured-yes" class="form-check-label">Yes</label>
+        </div>
+        <div class="form-check">
+            <input name="featured" type="radio" value="nay" id="featured-no" class="form-check-input" checked>
+            <label for="featured-no" class="form-check-label">No</label>
+        </div>
+    </div>
+
+    <div>
+        <input type="submit" value="Create Item" class="btn btn-primary">
+        <input type="submit" value="Cancel" class="btn btn-secondary">
+    </div>
+
+                                        </form>
+										<!-- End of form -->
+									</div>
+    
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Item List modal end --> 
 
 
 
