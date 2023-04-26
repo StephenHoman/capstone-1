49cb849131b072900141d9c45d2e659e71a0d748
StephenHoman <jakenesler@gmail.com>
2023-04-26 03:17:06 -0400

    small cleanup

--- search_page.php
+++ search_page.php
@@ -92,7 +92,7 @@ require_once "php_update_user.php";
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="user_page.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline">#<?php echo $_SESSION["id"]; ?> - <?php echo $_SESSION["username"]; ?> </span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
--- user_messages.php
+++ user_messages.php
@@ -86,7 +86,7 @@ require_once('php_messages.php');
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             
                         <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
+                        <a href="user_page.php" class="nav-link align-middle px-0">
                             <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"><?php echo $_SESSION["username"]; ?></span>
                         </a>
                     </li>
